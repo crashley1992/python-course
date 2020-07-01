a08b9fbd2ad166e19f2ad7e498c2e8350b6dc3fc
Ashley Clarke <crashley92@gmail.com>
2020-06-30 17:25:30 -0700

    built the start section, game start, and prompt to play

--- milestone-project-1/tic-tac.py
+++ milestone-project-1/tic-tac.py
@@ -1,14 +1,56 @@
-# visual representation
+# Player1 and Player Two assignments for X or O
+def players():
+    print('Player 1 choose if you want X or O')
+    selected_input = input()
+    while selected_input not in ['X', 'O']:
+        print('Select either X or O')
+        selected_input = input()
 
+    player_one = selected_input
+    if player_one == 'O':
+        print('Player one is O')
+        print('Player two is X')
+        player_two = 'X'
+    elif player_one  == 'X':
+        print('Player one is X')
+        print('Player two is O')
+        player_two = 'O'
+    
+    return player_one, player_two
 
-# User input for selection
+players()
+
+# visual representation 
+def display():
+    row1 = [' ', ' ', ' ']
+    row2 = [' ', ' ', ' ']
+    row3 = [' ', ' ', ' ']
+    print(row1)
+    print(row2)
+    print(row3)
+
+# Prompt if player is ready to play
+def start_game():
+    start_game == False
+    print('Are you ready to start? Y or N')
+    selected_input = input()
+    if selected_input == "Y":
+        start_game == True
+        display()
+# 
+    while selected_input not in ['Y', 'N']:
+            print('Select either Y or N')
+            selected_input = input()
+
+start_game()
+
+# User input for selection for each move
 
 
 # function for game
 
-
+# check if tie, or win
 
 # Updates after user input
 
 
-# new Visual
--- milestone-project-1/warmup.py
+++ milestone-project-1/warmup.py
@@ -30,8 +30,98 @@ row2[position_index]
 
 # Validate user input
 def user_choice():
-    choice = input('Please enter a number(0-10): ')
+    # Varaible
+
+    # inital 
+    choice = 'WRONG'
+    accetpable_range = range(0,10)
+    within_range = False
+
+    # Two Condirions to Check
+    # Digit or WITHIN_RANGE IS ==FALSE
+# isdigit keeps the loop going until the correct input is added
+    while choice.isdigit() == False or within_range == False:
+
+        choice = input('Please enter a number(0-10): ')
+         
+        # DIGIT CHECK
+        if choice.isdigit() == False:
+            print('That is not a digit between 0 and 10.')
+
+        # RANGE CHECK
+        if choice.isdigit() == True:
+            if int(choice) in accetpable_range:
+                within_range = True
+            else: 
+                print('Pick between 0-10')
+                within_range = False
+
+
     print(choice)
     return int(choice)
 
 user_choice()
+
+# result = 'WRONG VALUE'
+
+# accetpable_values = [0,1,2]
+# # output is false
+# result in accetpable_values
+
+# # output true
+# result not in accetpable_values
+
+
+game_list = [0,1,2]
+
+def display_game(game_list):
+    print('Here is the current list: ')
+    print(game_list)
+
+def position_choice():
+    choice = 'wrong'
+
+    while choice not in ['0','1','2']:
+        choice = input('Pick a position (0,1,2) :' )
+        
+        if choice not in ['0','1','2']:
+            print('Sorry invalid choice! ')
+        
+        return int(choice)
+
+def replacement_choice(game_list, position):
+    user_placement = input('Type a string to place at position: ')
+    game_list[position] = user_placement
+
+    return game_list
+
+replacement_choice(game_list,1)
+
+
+def game_on():
+    choice = 'wrong'
+
+    while choice not in ['Y', 'N']:
+        choice = input('Keep playing? Y or N' )
+        
+        if choice not in ['Y','N']:
+            print('Please choose Y or N  ')
+        
+        if choice == "Y":
+            return True
+        else: 
+            return False
+
+game_on = True
+game_list = [0,1,2]
+
+while game_on:
+    display_game(game_list)
+
+    position = position_choice()
+
+    game_list = replacement_choice(game_list,position)
+
+    display_game(game_list)
+
+    game_on = game_on()
